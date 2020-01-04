6c308b391f09f6f4da7d541b94beee7cd466551c
Maggie Greene <maggie.greene96@gmail.com>
2020-01-04 12:44:41 -0600

    Linked firebase for auth state changes

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,10 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
+import firebaseConnection from '../helpers/data/connection';
 import Home from '../components/pages/Home/Home';
 import NewBoard from '../components/pages/NewBoard/NewBoard';
 import Auth from '../components/pages/Auth/Auth';
@@ -23,9 +26,25 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
+firebaseConnection();
+
 class App extends React.Component {
   state = {
-    authed: true,
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
