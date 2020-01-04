8af107d7351d1b11c8433ac4c584b635d750086f
Maggie Greene <maggie.greene96@gmail.com>
2020-01-04 12:53:45 -0600

    User can authenticate through Google

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (event) => {
+    event.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
       <div className="Auth">
         <h1>Auth Page</h1>
+        <button className="btn btn-light" onClick={this.loginClickEvent}>Login With Google</button>
       </div>
     );
   }
